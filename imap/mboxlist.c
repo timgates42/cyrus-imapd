3ddd4dbe3ba944abf63b3c82552ad20bd4b6a21c
Ken Murchison <murch@fastmail.com>
2020-07-24 12:32:23 -0400

    mboxlist.c: calls to mboxlist_sublist() expect a non-NULL strarray_t*

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -4827,10 +4827,6 @@ EXPORTED strarray_t *mboxlist_sublist(const char *userid)
     mboxlist_closesubs(subs);
 
 done:
-    if (r) {
-        strarray_free(list);
-        return NULL;
-    }
     buf_free(&key);
     return list;
 }
